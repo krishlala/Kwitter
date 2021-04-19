d7b31d9dcfd17b4d352653f7e1d431c319a39f09
krishlala <71400038+krishlala@users.noreply.github.com>
2021-04-19 12:14:38 -0400

    Update kwitter_rooms.js

--- kwitter_rooms.js
+++ kwitter_rooms.js
@@ -1,15 +1,15 @@
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-      apiKey: "AIzaSyCJfxIN8dBJ775_OxCHPyE3kMaCxCuBJj4",
-      authDomain: "kwitter-34068.firebaseapp.com",
-      projectId: "kwitter-34068",
-      storageBucket: "kwitter-34068.appspot.com",
-      messagingSenderId: "196673916122",
-      appId: "1:196673916122:web:0f3f35893a32c2cd7229e3"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDyC545eHD6mJbhl58f2L9u9SQRpajfgGA",
+    authDomain: "kwitter-68077.firebaseapp.com",
+    projectId: "kwitter-68077",
+    storageBucket: "kwitter-68077.appspot.com",
+    messagingSenderId: "243452173023",
+    appId: "1:243452173023:web:ad761df00fa6e77c53676a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
